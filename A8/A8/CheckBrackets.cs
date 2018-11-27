cb49426e183f22859d06a03c864e2009f990615c
AlirezaAzizi <41ir92a@gmail.com>
2018-11-27 22:05:00 +0330

    A8

---
+++ A8/A8/CheckBrackets.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A8
+{
+    public class CheckBrackets : Processor
+    {
+        public CheckBrackets(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<string, long>)Solve);
+
+        public long Solve(string str)
+        {
+            Stack<Bracket> stackOfBrackets = new Stack<Bracket>();
+            char[] arrayOfBrackects = str.ToCharArray();
+            for (int i = 0; i < arrayOfBrackects.Length; i++)
+            {
+                if(arrayOfBrackects[i] == '(' || arrayOfBrackects[i] == '[' || arrayOfBrackects[i] == '{' )
+                {
+                    stackOfBrackets.Push(new Bracket(arrayOfBrackects[i] , i));
+                }
+
+                else if(
+                    ( stackOfBrackets.Count != 0 && arrayOfBrackects[i] == ')' && stackOfBrackets.Peek().Type == '(' ) ||
+                    ( stackOfBrackets.Count != 0 && arrayOfBrackects[i] == ']' && stackOfBrackets.Peek().Type == '[' ) ||
+                    ( stackOfBrackets.Count != 0 && arrayOfBrackects[i] == '}' && stackOfBrackets.Peek().Type == '{' ) 
+                    )
+                    {
+                        stackOfBrackets.Pop();
+                    }
+                    else if(arrayOfBrackects[i] == ')' || arrayOfBrackects[i] == ']' || arrayOfBrackects[i] == '}')
+                    {
+                        return i + 1;
+                    }
+            }
+            if(stackOfBrackets.Count != 0)
+            {
+                return stackOfBrackets.Peek().Index + 1;
+            }
+            return -1;
+        }
+        class Bracket
+        {
+            public Bracket(char type , long index)
+            {
+                this.Type = type;
+                this.Index = index;
+            }
+            public char Type { set; get; }
+            public long Index { set; get; }
+        }
+    }
+}
---
+++ A8/A8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeHeight t = new TreeHeight("");
+            Console.WriteLine(t.Solve(5, new long[] { 4, -1, 4, 1, 1 })); 
+        }
+       
+        
+    }
+}
